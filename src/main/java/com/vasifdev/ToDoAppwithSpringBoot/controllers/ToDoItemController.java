07a26d7bb6014af36fd65a52db4b947d9b095eb4
VasifVahidov <vasif202@gmail.com>
2023-01-30 15:10:36 +0100

    initial commit

---
+++ src/main/java/com/vasifdev/ToDoAppwithSpringBoot/controllers/ToDoItemController.java
@@ -0,0 +1,12 @@
+package com.vasifdev.ToDoAppwithSpringBoot.controllers;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+import java.util.logging.Logger;
+
+@Controller
+public class ToDoItemController {
+    private final Logger log = LoggerFactory.getLogger(ToDoItemController.class);
+    
+}
