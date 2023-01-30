15eb91425d07cc48bade0883b1131311f9219881
VasifVahidov <vasif202@gmail.com>
2023-01-30 15:49:21 +0100

    added index template

--- src/main/java/com/vasifdev/ToDoAppwithSpringBoot/controllers/ToDoItemController.java
+++ src/main/java/com/vasifdev/ToDoAppwithSpringBoot/controllers/ToDoItemController.java
@@ -1,13 +1,21 @@
 package com.vasifdev.ToDoAppwithSpringBoot.controllers;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.logging.Logger;
+//import java.util.logging.Logger;
 
 @Controller
 public class ToDoItemController {
-    private final Logger log = LoggerFactory.getLogger(ToDoItemController.class);
-    
+    private final Logger logger = LoggerFactory.getLogger(ToDoItemController.class);
 
+    @GetMapping("/")
+    public ModelAndView index() {
+        logger.debug("request to GET index");
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
 }
