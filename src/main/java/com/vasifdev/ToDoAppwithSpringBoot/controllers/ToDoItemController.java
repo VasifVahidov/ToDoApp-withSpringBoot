450e894afb05ba06da5b59bdd7a9218c1d987bf0
VasifVahidov <vasif202@gmail.com>
2023-02-17 09:31:47 +0100

    Update and Delete button initiated

--- src/main/java/com/vasifdev/ToDoAppwithSpringBoot/controllers/ToDoItemController.java
+++ src/main/java/com/vasifdev/ToDoAppwithSpringBoot/controllers/ToDoItemController.java
@@ -1,12 +1,21 @@
 package com.vasifdev.ToDoAppwithSpringBoot.controllers;
 
+import com.vasifdev.ToDoAppwithSpringBoot.models.ToDoItem;
 import com.vasifdev.ToDoAppwithSpringBoot.repositories.ToDoItemRepository;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.net.BindException;
+import java.time.Instant;
 
 //import java.util.logging.Logger;
 
@@ -24,5 +33,16 @@ public class ToDoItemController {
         modelAndView.addObject("todoItems", toDoItemRepository.findAll());
 
         return modelAndView;
+    }
+
+    @PostMapping("/todo/{id}")
+    public String updateToDoItem(@PathVariable("id") long id, @Valid ToDoItem toDoItem, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            toDoItem.setId(id);
+            return "update-todo-item";
+        }
+        toDoItem.setModifiedDate(Instant.now());
+        toDoItemRepository.save(toDoItem);
+        return "redirect:/";
     }
 }
